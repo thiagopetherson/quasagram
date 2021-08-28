00d4207469bdd5d70176c63304f4fb12534ec693
thiagopetherson <thiagopetherson@outlook.com>
2021-08-28 18:04:11 -0300

    prosseguindo com o front da camera e iniciando a criação do backend em laravel

---
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response; // Chamando a classe de respostas (como as de erros por exemplo)
+
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {        
+        $data = Post::orderBy('created_at','desc')->get();        
+        return response()->json(['posts' =>  $data], Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        $dataform = [];            
+        $dataform['caption'] = $request->caption;
+        $dataform['location'] = $request->location;       
+
+        $post = new Post;
+
+
+        $file = $request->file('image');        
+
+        // Se informou o arquivo, retorna um boolean
+        $fileExists = $request->hasFile('image');
+        if(!$fileExists) {
+            return response()->json(['error' => 'Não é um arquivo de imagem válido!'], 404);
+        }
+
+        // Retorna mime type do arquivo (Exemplo image/png)
+        $mimeType = $request->image->getMimeType();
+
+        $arrayMimeTypes = ['image/png','image/jpg','image/jpeg'];
+
+        if(!in_array($mimeType,  $arrayMimeTypes))
+        {
+            return response()->json(['error' => 'Não é um arquivo de imagem válido!'], 404);
+        }
+
+        // Extensão do arquivo
+        $extension = $request->image->getClientOriginalExtension();
+
+        $arrayExtensions = ['PNG','JPG','JPEG','png','jpg','jpeg'];
+
+        if(!in_array($extension, $arrayExtensions))
+        {
+            return response()->json(['error' => 'O arquivo não tem uma extensão válida!'], 404);
+        }
+
+        // Tamanho do arquivo
+        $fileSize = $request->image->getSize();
+
+        if($fileSize > 2097152) {
+            return response()->json(['error' => 'O tamanho do arquivo ultrapassa o limite de 2MB!'], 404);
+        }
+
+        //Verificando se é um arquivo válido
+        $isValid = $request->file('image')->isValid();
+
+        if($isValid != 1) {
+            return response()->json(['error' => 'Não é um arquivo de imagem válido!'], 404);
+        }        
+
+        $imageName = md5(rand(1, 9999999)) . '.jpg';
+
+        //Movendo o arquivo da pasta temporária para storage (que tem um espelho em public)
+        $upload = $request->image->storeAs('media/uploadsimagesprofile', $imageName);
+
+        $dataform['image'] = $upload;
+
+        if($upload AND $post->create($dataform)) {            
+             return response()->json(['success' => 'Imagem Inserida com Sucesso'], Response::HTTP_OK);
+        }
+
+        return response()->json(['error' => 'A imagem não foi inserida. Tente novamente ou entre em contato com o suporte!'], 404);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- frontend/src/pages/PageCamera.vue
+++ frontend/src/pages/PageCamera.vue
@@ -8,16 +8,21 @@
 
     <div class="text-center q-pa-md">
 
-      <q-btn @click="captureImage" color="grey-10" icon="eva-camera" size="lg" round />
+      <q-btn v-if="hasCameraSupport" @click="captureImage" color="grey-10" icon="eva-camera" size="lg" round />
+      <q-file v-else v-model="imageUpload" @input="captureImageFallback" label="Chose a image" outlined accept="image/*">
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
 
       <div class="row justify-center q-ma-md">
         <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
       </div>
 
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.location" class="col col-sm-6" label="Location" dense>
+        <q-input v-model="post.location" :loading="locationLoading" class="col col-sm-6" label="Location" dense>
           <template v-slot:append>
-            <q-btn round dense flat icon="eva-navigation-2-outline" />
+            <q-btn v-if="!locationLoading && locationSupported" round dense flat icon="eva-navigation-2-outline" @click="getLocation" />
           </template>
         </q-input>
       </div>
@@ -36,6 +41,8 @@ import { defineComponent } from 'vue'
 import { uid } from 'quasar' // Gerador de ID's únicos
 require('md-gum-polyfill') // Instalamos a biblioteca Poliyfill (npm install --save md-gum-polyfill)
 
+// Web Api de localização https://geocode.xyz/
+
 export default defineComponent({
   name: 'PageCamera',
   data () {
@@ -47,7 +54,17 @@ export default defineComponent({
         photo: null,
         date: Date.now()
       },
-      imageCaptured: false
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true,
+      locationLoading: false
+    }
+  },
+  computed: {
+    locationSupported () {
+      // Verificando se o navegagor possui suporte a geolocalização
+      if ('geolocation' in navigator) return true
+      else return false
     }
   },
   methods: {
@@ -58,6 +75,9 @@ export default defineComponent({
       }).then(stream => {
         // Chamando o ref 'video' que demos a tag 'video'
         this.$refs.video.srcObject = stream
+      }).catch(error => {
+        // Entrará aqui no caso do usuário não tiver acesso a câmera (marcar aquele "não permitir", por exemplo)
+        this.hasCameraSupport = false
       })
     },
     captureImage () {
@@ -68,11 +88,109 @@ export default defineComponent({
       let context = canvas.getContext('2d')
       context.drawImage(video, 0, 0, canvas.width, canvas.height)
       this.imageCaptured = true
-      this.post.photo = canvas.toDataUrl() // Pegando os dados de base64
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL()) // Pegando os dados de base64
+      
+      this.disableCamera() // desativando a Câmera
+    },
+    captureImageFallback (file) {
+      // this.post.photo = file
+      this.post.photo = file.target.files[0]   
+      //console.log(file.target.files[0])
+
+      let canvas = this.$refs.canvas    
+      let context = canvas.getContext('2d')
+      
+      // Essa parte abaixo foi pega no stackoverflow (pelo professor)
+      // https://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
+      var reader = new FileReader()
+      reader.onload = event => {
+        var img = new Image()
+        img.onload = () => {
+          canvas.width = img.width
+          canvas.height = img.height
+          context.drawImage(img,0,0)
+
+          this.imageCaptured = true
+        }
+        img.src = event.target.result
+      }
+      // reader.readAsDataURL(file)
+      reader.readAsDataURL(file.target.files[0])
+
+    },
+    disableCamera () {
+      // desativando a Câmera
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop()
+      })
+    },
+    dataURItoBlob(dataURI) {
+      // ESSE MÉTODO FOI PEGO NO STACKOVERFLOW (INCLUSIVE O PROFESSOR QUE PEGOU)
+      // https://stackoverflow.com/questions/12168909/blob-from-dataurl
+
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], {type: mimeString});
+      return blob;
+
+    },
+    getLocation () {
+      this.locationLoading = true
+      navigator.geolocation.getCurrentPosition(position => {
+        this.getCityAndCountry(position)
+      }, err => {
+        console.log("Error: ", err)
+      }, { timeout: 7000 }) // Adicionamos um tempo para ser pega a localização
+    },
+    getCityAndCountry (position) {      
+      let apiUrl = `https://geocode.xyz/${ position.coords.latitude },${ position.coords.longitude }?json=1`
+      //let apiUrl = `http://maps.googleapis.com/maps/api/staticmap?center=+${ position.coords.latitude },${ position.coords.longitude }+&zoom=14&size=400x300&sensor=false`
+      this.$axios.get(apiUrl).then(result => {
+        this.locationSuccess(result)
+      }).catch(err => {
+        console.log("Err: ", err)
+        this.locationError()
+      })
+    },
+    locationSuccess (result) {
+      this.post.location = result.data.city
+      if (result.data.country) {
+        this.post.location += `, ${ result.data.country }`
+      }
+      this.locationLoading = false
+    },
+    locationError(){
+      this.$q.dialog({
+        title: 'Error',
+        message: 'Could not find your location.'
+      })
+      this.locationLoading = false
     }
-  },
+  },  
   mounted () {
-    this.initCamera()
+    this.initCamera()  
+  },
+  beforeDestroy () {
+    if (this.hasCameraSupport) {
+      this.disableCamera() // desativando a Câmera
+    }
   }
 })
 </script>
