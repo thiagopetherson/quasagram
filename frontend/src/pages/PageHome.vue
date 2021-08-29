4b3cd1df728a3b9629a711ee166e675f93605e2e
thiagopetherson <thiagopetherson@outlook.com>
2021-08-29 16:05:44 -0300

    Atualizando

--- frontend/src/pages/PageHome.vue
+++ frontend/src/pages/PageHome.vue
@@ -3,18 +3,37 @@
 
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card v-for="post in posts" :key="post.id" class="card-post q-mb-md" flat bordered>
-          
-          <q-separator />
+        
+        <template v-if="posts">
+          <q-card v-for="post in posts" :key="post.id" class="card-post q-mb-md" flat bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+              </q-item-section>         
 
-          <q-img :src="post.imageUrl" />
+              <q-item-section>
+                <q-item-label class="text-bold">thiago___petherson</q-item-label>
+                <q-item-label caption>{{ post.location }}</q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-card-section>
-            <div>{{ post.caption }}</div>
-            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-          </q-card-section>
-     
-        </q-card>
+            <q-separator />
+
+              <q-img :src="post.image" />
+            
+            <q-card-section>
+              <div>{{ post.caption }}</div>
+              <div class="text-caption text-grey">{{ post.created_at }}</div>
+            </q-card-section>                        
+          </q-card>
+        </template>
+
+        <template v-else>
+            <h5 class="text-center text-grey">Não temos posts</h5>
+        </template>
+       
       </div>
 
       <div class="col-4 large-screen-only">
@@ -41,47 +60,49 @@
 
 <script>
 import { date } from 'quasar'
+import axios from 'axios' // Importando Axios
+
 
 export default {
   name: 'PageHome',
   data () {
     return {
-      posts: [
-        {
-          id: 1,
-          caption: 'Golden Gate bridge',
-          date: 1591776655504,
-          location: 'San Francisco, United States',
-          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        },
-        {
-          id: 3,
-          caption: 'Golden Gate bridge',
-          date: 1591776655504,
-          location: 'San Francisco, United States',
-          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        },
-        {
-          id: 3,
-          caption: 'Golden Gate bridge',
-          date: 1591776655504,
-          location: 'San Francisco, United States',
-          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        },
-        {
-          id: 4,
-          caption: 'Golden Gate bridge',
-          date: 1591776655504,
-          location: 'San Francisco, United States',
-          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        }
-      ]
+      posts: []
     }
+  },
+  methods: {
+    getPosts () {
+      this.$q.loading.show({ // Chamando o loading
+        delay: 400
+      })
+      axios.get('http://127.0.0.1:8000/api/posts') // Fazendo a requisição no API
+        .then((resp) => {
+          console.log(resp.data.posts)        
+          this.posts = resp.data.posts
+          this.$q.loading.hide() // Encerrando o loading
+        })
+        .catch((err) => {
+          // Chamando o plugin de notificação
+          this.$q.notify({
+            position: 'center', // Posição que a mensagem aparecerá
+            timeout: 3000, // Tempo de exposição da mensagem no browser (3000 = 3 segundos)
+            color: 'negative', // Cor do background da mensagem
+            textColor: 'white', // Cor do texto da mensagem
+            actions: [{ icon: 'check', color: 'white' }], // Ícone e cor do ícone
+            message: 'Erro na requisição. Erro: ' + err // Texto da mensagem
+          })
+          this.$q.loading.hide() // Encerrando o loading
+        })
+    } 
   },
   filters: {
     niceDate(value) {
+      // Formatar a data
       return date.formatDate(value, 'MMMM D h:mmA')
     }
+  },
+  created () {
+    this.getPosts()
   }
 }
 </script>
